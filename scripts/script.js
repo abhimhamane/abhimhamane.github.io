bb5289ae157675e397c10cdaaa1ef59f2254f2c2
abhimhamane <abhi.mhamane30@gmail.com>
2022-06-02 00:59:38 +0530

    styles added

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,44 @@
-function demoFunc() {
-    alert("all set to go!!")
-}
+/* Code for changing active 
+            link on clicking */
+            var btns = 
+                $("#navigation .navbar-nav .nav-link");
+  
+            for (var i = 0; i < btns.length; i++) {
+                btns[i].addEventListener("click",
+                                      function () {
+                    var current = document
+                        .getElementsByClassName("active");
+  
+                    current[0].className = current[0]
+                        .className.replace(" active", "");
+  
+                    this.className += " active";
+                });
+            }
+  
+            /* Code for changing active 
+            link on Scrolling */
+            $(window).scroll(function () {
+                var distance = $(window).scrollTop();
+                $('.page-section').each(function (i) {
+  
+                    if ($(this).position().top 
+                        <= distance + 250) {
+                          
+                            $('.navbar-nav a.active')
+                                .removeClass('active');
+  
+                            $('.navbar-nav a').eq(i)
+                                .addClass('active');
+                    }
+                });
+            }).scroll();
 
-document.onload = demoFunc()
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+    }
